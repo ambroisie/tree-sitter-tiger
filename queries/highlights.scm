d83fa3acb7e9d578fb811d3f2a8ca8b1d036a439
Bruno BELANYI <bruno@belanyi.fr>
2022-06-02 22:27:42 +0200

    Add keywords highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,5 +1,28 @@
 (comment) @comment
 
+; Keywords {{{
+[
+  "array"
+  (break_expression)
+  "do"
+  "else"
+  "end"
+  "for"
+  "function"
+  "if"
+  "import"
+  "in"
+  "let"
+  "of"
+  "primitive"
+  "then"
+  "to"
+  "type"
+  "var"
+  "while"
+] @keyword
+; }}}
+
 ; Literals {{{
 (nil_literal) @constant.builtin
 (integer_literal) @number
