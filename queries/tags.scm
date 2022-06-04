746c8d27b8a389d1b5806164a554c6c7b1ceb5ce
Bruno BELANYI <bruno@belanyi.fr>
2022-06-04 21:42:23 +0200

    Add method tags

--- queries/tags.scm
+++ queries/tags.scm
@@ -8,4 +8,12 @@
   function: (identifier) @name) @reference.call
 ; }}}
 
+; Methods {{{
+(method_declaration
+  name: (identifier) @name) @definition.method
+
+(method_call
+  method: (identifier) @name) @reference.call
+; }}}
+
 ; vim: sw=2 foldmethod=marker
