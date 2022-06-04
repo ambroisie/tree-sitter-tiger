2bd24cceb42fd4d714efcdeaa0d69afac725839d
Bruno BELANYI <bruno@belanyi.fr>
2022-06-04 10:22:22 +0200

    Add folds
    Unfortunately it seems like this is not really testable with the
    'tree-sitter' tooling.
    
    If I want to check that this is working correctly, I should explore how
    to make use of, e.g., neovim to automate testing.

---
+++ queries/folds.scm
@@ -0,0 +1,18 @@
+[
+  (array_expression)
+  (record_expression)
+  (sequence_expression)
+  (if_expression)
+  (while_expression)
+  (for_expression)
+  (let_expression)
+  (function_declaration)
+  (primitive_declaration)
+] @fold
+
+[
+ (comment)
+ (string_literal)
+] @ignore
+
+; vim: sw=2 foldmethod=marker
