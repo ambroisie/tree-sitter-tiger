c9efe870ab6372855bc37a73dd9d66f38ac6b2f0
Bruno BELANYI <bruno@belanyi.fr>
2022-06-04 21:50:45 +0200

    Add class tags

--- queries/tags.scm
+++ queries/tags.scm
@@ -8,6 +8,19 @@
   function: (identifier) @name) @reference.call
 ; }}}
 
+; Class {{{
+(class_declaration
+  name: (identifier) @name) @definition.class
+(type_declaration
+  name: (identifier) @name
+  (class_type)) @definition.class
+
+(new_expression
+  class: (type_identifier) @name) @reference.class
+(extends_qualifier
+  super: (type_identifier) @name) @reference.class
+; }}}
+
 ; Methods {{{
 (method_declaration
   name: (identifier) @name) @definition.method
