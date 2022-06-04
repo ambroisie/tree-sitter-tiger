a87126399638283c880c60a3d3ed349d2e73e32f
Bruno BELANYI <bruno@belanyi.fr>
2022-06-04 10:32:13 +0200

    Add function text objects

--- queries/textobjects.scm
+++ queries/textobjects.scm
@@ -1,3 +1,9 @@
+; Functions {{{
+ (function_declaration
+   body: (_) @function.inner) @function.outer
+ (primitive_declaration) @function.outer
+; }}}
+
 ; Misc {{{
 (comment) @comment.outer
 ; }}}
